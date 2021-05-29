45209c731b2bc88c0a0b616df2cc43b67f8e2077
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 08:04:01 -0300

    Add files via upload

---
+++ 16.If Else Statement Example.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  9 20:49:31 2020
+
+@author: utob
+"""
+
+    
+    
+ #examplefor python if else statement
+
+marks = int(input("please enter your subject marks:"))
+if marks >=50:
+    print("congratulation")#s1
+    print("you cleared the subject")#s2
+else:
+    print("you failed")#s3
+    print("better luck next time")#s4
+    
+
+    
