c899ef3c28e7497df25f0d66ed0f0c3ef91cf190
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 07:27:37 -0300

    Add files via upload

---
+++ 2.perimeter of rectangle bar dovom.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  5 17:22:43 2020
+
+@author: utob
+"""
+
+# wrighting a perimeter of the rectangle #
+
+langth =float(input("please enter the langth:"))
+width=float(input("please enter the width: "))
+
+#caculate the area #
+
+
+perimeter = 2 * (langth + width)
+print("the number of the rectangle is:", langth ,"+", width ,"=", perimeter ) 
