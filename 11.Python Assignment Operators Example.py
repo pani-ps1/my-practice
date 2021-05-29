e779d549a49bf267f62d8eec621fbe1825e100aa
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 08:02:36 -0300

    Add files via upload

---
+++ 11.Python Assignment Operators Example.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  9 11:24:22 2020
+
+@author: utob
+"""
+
+a=7
+total=21
+total+=a#using+=operator
+print("the value of the total after using+=operator is:",total)
+total-=a#using-=operator
+print("the value of the total after using-=operator is:",total)
+total*=a#using*=operator
+print("the value of total after using-=operator is:",total)
+total**=a#using**=oprator
+print("the value of the total after using **=operator is:",total)
+total/=a#using/=oprator
+print("the value of the total after using/=oprator is:",total)
+total%=a#using %=oprator
+print("the value of the total after using% =oprator is:",total)
+x=9
+y=65
+x&=y#using &=operator
+print("the value of the x after using &=oprator is:",x)
+x|=9#using|=oprator
+print("the value of the x after using |=oprator is:",x)
+x^=y#using ^=oprator
+print("the value of the x after using ^=oprator:",x)
