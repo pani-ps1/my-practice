344cd25d80bd01b7e222dbf33f1a8f7eb84e3a44
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 08:03:27 -0300

    Add files via upload

---
+++ 14.Logical Operators Example.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  9 19:04:17 2020
+
+@author: utob
+"""
+
+age = 29
+#logical AND example 
+
+if age<33 and age >20:
+    print("young man")
+else:
+    print("no eligible")
+    
+#logical or example
+if age <18 or age>80:
+    print("not eligible to work")
+else:
+    print("please forward your resume")
+    
+    
+    
