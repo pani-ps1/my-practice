4fdc2a1bc062cbd51553508979fb22fd77f0d471
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 07:28:25 -0300

    Add files via upload

---
+++ 5.add two number together.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  5 18:09:03 2020
+
+@author: utob
+"""
+
+#python program to add two numbers#
+
+number1 = input ("please enter the first number:")
+number2 = input ("please enter the second number:")
+
+#using arithmetic + operator to add two numbers#
+sum = float (number1)+float (number2)
+print ('the sum of {0} and {1} is {2}'.format(number1, number2,sum))
