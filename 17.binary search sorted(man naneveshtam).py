f0046366c55b735f17551f9f0f2d619748294f6c
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 08:04:20 -0300

    Add files via upload

---
+++ 17.binary search sorted(man naneveshtam).py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  9 21:04:45 2020
+
+@author: utob
+"""
+
+def binarySearch(arr, target):
+    left = 0
+    right = len(arr)-1
+    while left <= right:
+
+        mid = (left+right)//2
+
+        # Check if x is present at mid
+        if arr[mid] == target:
+            return mid
+
+        # If x is greater, ignore left half
+        elif arr[mid] < target:
+            left = mid + 1
+
+        # If x is smaller, ignore right half
+        else:
+            right = mid - 1
+
+    # If we reach here, then the element was not present
+    return -1
+
+
+arr = [1, 2, 3, 4, 5, 6]
+target = 6
+
+result = binarySearch(arr, target)
+
+if result != -1:
+    print("Element is present at index %d" % result)
+else:
+    print("Element is not present in array")
