93446da1cdcd6d3fb1d4f1ca113a4225e88d3c00
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 07:27:15 -0300

    Add files via upload

---
+++ 1. perimeter rectangle.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  5 17:22:20 2020
+
+@author: utob
+"""
+
+#python write a program to find the perimeter of rectangle#
+
+length = float(input('please enter the lenght:'))
+width = float(input('please enter the width:'))
+
+#calculate the area
+
+
+perimeter = 2*(length + width)
+print("perimeter of a rectangle using", length ,"and", width , "=" ,perimeter)
+    
