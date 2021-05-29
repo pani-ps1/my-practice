92881dd67bda8bc3063ef0779951ce38c1132276
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 08:02:53 -0300

    Add files via upload

---
+++ 12.Python Bitwise Operators.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  9 12:11:11 2020
+
+@author: utob
+"""
+
+a=9
+b=65
+print("bitwise and oprator on 9 and 65 is=",a&b)
+print("bitwise OR oprator on 9 and 65 is =",a|b)
+print("bitwise EXCLUSIVE or operator on 9 and 65 is =",a^b)
+print("bitwise not oprator on 9 is =",~a)
+
+print("bitwise left shift operator on 9 is =",a<<1)
+print("bitwise right shift operator on 65 is =",b>>1)
+
+-------------------------------
+Lets see the Python calculations
+Bitwise AND Operation = a & b
+00001001 & 01000001 = 00000001 = 1
+
+Bitwise OR Operation = a | b
+00001001 | 01000001 = 01001001 = 73
+
+Bitwise Exclusive OR Operation in Python = a ^ b
+00001001 ^ 01000001 = 01001000 = 72
+
+Right Shift Operation in Python = b >> 1
+01000001 >> 1 = 00100000 = 32
