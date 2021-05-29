947c7c1d4c153b1bfefca1a4bf167605c1bac465
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 08:03:49 -0300

    Add files via upload

---
+++ 15.Python If Statement.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  9 20:40:38 2020
+
+@author: utob
+"""
+
+#example for python if statment
+
+number = int(input("please enter any integer value:"))
+if number>=1:
+    print("you have entered integer")
+    
+#example for python if statment
+
+number =int(input("please enter any integer value:"))
+if number >=1:
+    print ("you have entered positive integer")
+print("this message is not coming from python if statment")
+    
