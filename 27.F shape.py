e43fc216e5b59d7f034d9efa2b61d5dd8066b79f
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 08:08:35 -0300

    Add files via upload

---
+++ 27.F shape.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 29 09:22:37 2020
+
+@author: utob
+"""
+
+numbers = [5, 2, 5, 2, 2]
+
+#khodam neveshtam
+
+#for  x in range (6):
+   # if numbers in range (numbers):
+    #    print ({x})
+        
+##rah hal sade tar
+#in rah faghat baraye python javab mide
+
+#for x_count in numbers:
+  #  print('x'* x_count) #
+
+# rah hal dorost
+for x_count in numbers:
+    output = ''
+    for count in range(x_count):
+        output +='x'
+    print(output)
+        
