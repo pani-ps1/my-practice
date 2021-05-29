00db66de0c253379840765d16a0f9a246361c626
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 08:07:43 -0300

    Add files via upload

---
+++ 25.for loops.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 29 09:06:51 2020
+
+@author: utob
+"""
+
+prices = [10, 20, 30]
+
+total = 0
+for price in prices:
+    total +=price
+print(f"Total: {total}")
