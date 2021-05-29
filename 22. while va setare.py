b0c6bcf7632e26978f28e008b2b03276da8c71ab
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 08:06:35 -0300

    Add files via upload

---
+++ 22. while va setare.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 28 21:13:21 2020
+
+@author: utob
+"""
+# baresi sharayet while hast va inke
+#mitunimm maslan ye shekl ham ezafe konim va tekraresh koim
+
+i = 1
+while i <= 5:
+    print ('*'*i)
+    i = i+1
+print ("Done")
+     
