15a009391db51c529e36e6aff9032fb3d304e02f
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 08:04:51 -0300

    Add files via upload

---
+++ 19.graeter than.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 28 20:15:53 2020
+
+@author: utob
+"""
+
+temperature = 35
+
+if temperature >30:
+    print("Its a hot day")
+else:
+    print("Its not a hot day")
+    
