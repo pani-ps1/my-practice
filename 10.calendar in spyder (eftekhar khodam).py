40c6e27320b70e3906f24f5787db5f4932f7da6e
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 08:02:15 -0300

    Add files via upload

---
+++ 10.calendar in spyder (eftekhar khodam).py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  8 16:30:30 2020
+
+@author: utob
+"""
+
+import calendar
+yy = int(input("enter year:"))
+mm = int (input("enter month:"))
+print(calendar.month(yy,mm))
