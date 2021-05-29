f39755d66814c1ba3adb7b4ba72386a836f40b80
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 08:09:09 -0300

    Add files via upload

---
+++ 29.2d LISTS.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 29 11:28:24 2020
+
+@author: utob
+"""
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+    ]
+#matrix[0][1]=20
+#print(matrix[0][1])
+
+#
+for row in matrix:
+    for item in row:
+        print(item)
