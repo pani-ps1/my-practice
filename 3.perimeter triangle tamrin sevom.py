35de869fe9bcbfacefa86a129f9cd2cc8b0d2aea
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 07:27:54 -0300

    Add files via upload

---
+++ 3.perimeter triangle tamrin sevom.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  5 18:01:23 2020
+
+@author: utob
+"""
+
+#wrighting the area of the rectangle#
+
+langth = float (input ("please enter the langth:"))
+width = float (input ("please enter the width:"))
+
+#caculating the area#
+perimeter =2*(langth +width)
+
+print("the number of perimeter is:",langth,"+",width,"=",perimeter)
