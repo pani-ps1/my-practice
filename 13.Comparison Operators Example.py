29dc9a14c1b7e2db9112759018386c5087cbaf89
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-29 08:03:07 -0300

    Add files via upload

---
+++ 13.Comparison Operators Example.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  9 17:39:17 2020
+
+@author: utob
+"""
+
+a=9
+b=4
+print("the output of 9 > 4 is:",a>b)
+print("the output of 9 < 4 is :",a<b)
+print("the output of 9<=4 is: ",a<=b)
+print("the output of 9>=4 is:",a>=b)
+print("the output of 9 equal to 4 is:",a==b)
+print("the output of 9 not equal to is:",a!=b)
+
+
+x=10
+y=25
+if x>=y:
+ print("x value is greater than or equal to y")
+else:
+    print("x value is less than or equal to y")
+    
